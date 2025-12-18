240d06aa920d381495f467d6335fe769718105a2
Sasha Odumodu <189272304+sashaodumodu@users.noreply.github.com>
2025-12-18 19:14:55 +0000

    please work HAHAH

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -52,6 +52,6 @@ if __name__ == "__main__":
 
         if choice == 'exit':
             console.print("[red]You decide to leave the forest. Goodbye![/red]")
-            break #please work
+            break #second check
 
         show_result(step(choice, events))
